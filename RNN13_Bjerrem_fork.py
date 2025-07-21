d526b8084c7f94498122d02303ac30935e3d52e1
Mark James Thompson <622042+Amuraivel@users.noreply.github.com>
2025-07-21 20:55:54 +0300

    Delete RNN13_Bjerrem_fork.py
    Stub file not needed

--- RNN13_Bjerrem_fork.py
+++
@@ -1,5 +0,0 @@
-# This is a fork of the data and model 23 in a simplified way to compare output with Model 23
-import pandas as pd
-
-data = pd.read_csv("Example_data/Sutherland_DHFR.csv")
-print(data.head())
